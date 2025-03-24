5699a93d9a8d64e85d8bd25d4340b44f39f1a93e
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-03-24 16:54:29 +0900

    オプションの指定方法を変更し、例外処理を追加

--- calendar/main.rb
+++ calendar/main.rb
@@ -7,17 +7,42 @@ require_relative 'annual_calendar'
 
 options = {}
 option_parser = OptionParser.new
-option_parser.on('-y VAL', Integer) do |year|
-  options[:year] = year
-end
-option_parser.on('-m VAL', Integer) do |month|
+option_parser.on('-m MONTH', Integer) do |month|
+  raise OptionParser::InvalidArgument, month if !month.nil? && !(1..12).cover?(month)
+
   options[:month] = month
 end
-option_parser.parse!
+option_parser.on('-y [YEAR]', Integer) do |year|
+  if year.nil?
+    options[:year] = Time.now.year
+  else
+    raise OptionParser::InvalidArgument, year unless (1..9999).cover?(year)
 
-if options[:year] && !options[:month]
-  puts AnnualCalendar.new(options[:year]).generate
-else
-  now = Time.now
-  puts Calendar.new(options[:year] || now.year, options[:month] || now.month).generate
+    options[:year] = year
+  end
 end
+
+argv = []
+begin
+  argv = option_parser.parse!(ARGV)
+rescue OptionParser::InvalidArgument => e
+  abort "Error: Month '#{e.args[1]}' must be an integer between 1 and 12" if e.args[0] == '-m'
+  abort "Error: Year '#{e.args[1]}' must be an integer between 1 and 9999" if e.args[0] == '-y'
+rescue OptionParser::MissingArgument => e
+  abort "Error: Argument is missing #{e.args[0]}"
+end
+
+abort 'Error: Option -m and -y cannot be specified together' if options[:year] && options[:month]
+
+if options[:year]
+  abort 'Error: Too many arguments for -y (Only one argument is allowed)' unless argv.empty?
+  puts AnnualCalendar.new(options[:year]).generate
+  exit
+end
+
+if options[:month]
+  abort 'Error: Too many arguments for -m　(Only month and year are allowed)' if argv.size > 1
+  abort "Error: Year '#{argv[0]}' must be an integer between 1 and 9999" if !argv[0].nil? && (!argv[0].match?(/^\d+$/) || !(1..9999).cover?(argv[0].to_i))
+end
+
+puts Calendar.new(argv[0]&.to_i || Time.now.year, options[:month] || Time.now.month).generate
