bc63ab014aaf7874b14bd8a4a15a07e6488839aa
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-08 15:56:52 +0900

    longフォーマット表示を実装

--- ls/entry.rb
+++ ls/entry.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'etc'
+require_relative 'file_mode_formatter'
+
 class Entry
   attr_reader :display_name
 
@@ -7,4 +10,21 @@ class Entry
     @display_name = display_name || path
     @stat = File::Stat.new(path) if File.exist?(path)
   end
+
+  def mode = FileModeFormatter.format(@stat.mode)
+
+  def extended_attribute
+    # todo 拡張属性の取得
+    ' '
+  end
+
+  def nlink = @stat.nlink
+
+  def owner_name = Etc.getpwuid(@stat.uid).name
+
+  def group_name = Etc.getgrgid(@stat.gid).name
+
+  def file_size = @stat.size
+
+  def modified_date = @stat.mtime
 end
--- ls/formatter/long_formatter.rb
+++ ls/formatter/long_formatter.rb
@@ -6,6 +6,28 @@ class LongFormatter
   end
 
   def generate_content
-    "TODO content of LongFormatter"
+    delimiter = ' '
+    @container.entries.map do |entry|
+      [
+        entry.mode,
+        entry.extended_attribute + delimiter,
+        entry.nlink.to_s.rjust(@container.max_nlink_digits) + delimiter,
+        entry.owner_name.ljust(@container.max_owner_name_length) + delimiter * 2,
+        entry.group_name.ljust(@container.max_group_name_length) + delimiter * 2,
+        entry.file_size.to_s.rjust(@container.max_file_size_digits) + delimiter,
+        format_modified_date(entry.modified_date) + delimiter,
+        entry.display_name
+      ].join
+    end.join("\n")
+  end
+
+  private
+
+  def format_modified_date(date)
+    if date.year == Time.now.year
+      date.strftime('%_2m %_2d %H:%M')
+    else
+      date.strftime('%_2m %_2d  %Y')
+    end
   end
 end
