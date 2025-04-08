9fc1421d9d033fca1b45716311b42c3ecd478fc2
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-08 16:54:30 +0900

    引数にファイルを表示した場合の並び順を修正

--- ls/container/existent_file_path_container.rb
+++ ls/container/existent_file_path_container.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'container_statistics'
+require_relative '../optional_argument'
 
 class ExistentFilePathContainer
   include ContainerStatistics
@@ -15,6 +16,7 @@ class ExistentFilePathContainer
   private
 
   def generate_entries
-    @entries = @paths.map { |path| Entry.new(path) }
+    sorted_entries = @paths.sort.map { |path| Entry.new(path) }
+    @entries = OptionalArgument.instance.reverse_order? ? sorted_entries.reverse : sorted_entries
   end
 end
